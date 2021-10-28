f0e0a093e0d2a91789dec5f596263651216834f5
rominamenacho <43242020+rominamenacho@users.noreply.github.com>
2021-10-28 20:10:33 -0300

    .

---
+++ ConectionSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class ConectionSingleton
+    {
+        private static ConectionSingleton UniqueInstance = null;
+
+        private ConectionSingleton() { }
+
+        public static ConectionSingleton getInstance() {
+            if (UniqueInstance == null) {
+
+                UniqueInstance = new ConectionSingleton();
+            }
+            return UniqueInstance;
+        }
+
+        public void MethodOne()
+        {
+            Console.WriteLine( "im the method one");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ConectionSingleton c = new ConectionSingleton(); no es posible gracias al constructor privado
+
+            ConectionSingleton c = ConectionSingleton.getInstance(); //lo permite porque es static
+            c.MethodOne();
+            //ConectionSingleton c2 = ConectionSingleton.getInstance();
+
+            Console.WriteLine("c Is unique instance? " + (c is ConectionSingleton) );
+            //Console.WriteLine("c2 Is unique instance? " + (c2 is ConectionSingleton));
+
+            Console.ReadKey();
+        }
+    }
+}
